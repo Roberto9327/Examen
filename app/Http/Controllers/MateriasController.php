f35e9bb8e9ef75fc2ea00cf576ce05cbe01c0899
Roberto <rrrr.22.1993@gmail.com>
2021-03-19 20:21:11 -0400

    Examen2

---
+++ app/Http/Controllers/MateriasController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\materias;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class MateriasController extends Controller
+{
+    public function index()
+    {
+        $materias = DB::table('Materia')
+        ->select('*')
+        ->where('state','=','1')
+        ->paginate(5);
+        return view('materias/index',compact('materias'));
+    }
+}
